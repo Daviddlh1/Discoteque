059a036cb879f9d214b70e8a2db46d9f61834c78
David De la Hoz <Daviddlh1@bitbucket.org>
2023-07-15 18:37:50 -0500

    creating layers and services

--- Discoteque.API/Program.cs
+++ Discoteque.API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Discoteque.Data;
+using Discoteque.Business.Service;
+using Discoteque.Business.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// AddScoped quiere decir que solo se va a instanciar cuando se necesite.
+builder.Services.AddScoped<IArtistService ,ArtistService>();
+
+builder.Services.AddDbContext<DiscotequeContext>(
+    opt => opt.UseInMemoryDatabase("Discoteque")
+);
 
 var app = builder.Build();
 
